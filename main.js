5e5abaa57fa8e355b42464cf1d270b4354fa966a
calebniju <96456763+calebniju@users.noreply.github.com>
2022-09-13 17:31:03 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+Webcam.set({
+width:300,
+height:300,
+image_format : 'png',
+png_quailty:90
+});
+
+
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        
+    });
+}
+console.log('ml5 version;',ml5.version);
+
+classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/Mjq8KYjvU/model.json',modelloaded);
